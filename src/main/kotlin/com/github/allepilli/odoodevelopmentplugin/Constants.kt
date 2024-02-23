d508ea10a6dcbb86e012bde3b2914cd84a4b9be3
AllePilli <peal@odoo.com>
2024-02-23 21:11:59 +0100

    [ADD] New Manifest file action & template
    Template can be found and edited in Settings/Editor/File and Code Templates under the Other tab. Creates new __manifest__.py files for modules by using a custom dialog to fill in common data of the manifest file

---
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/Constants.kt
@@ -0,0 +1,6 @@
+package com.github.allepilli.odoodevelopmentplugin
+
+object Constants {
+    const val MANIFEST_FILE_NAME = "__manifest__"
+    const val MANIFEST_FILE_WITH_EXT = "$MANIFEST_FILE_NAME.py"
+}
---
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/OdooIcons.kt
@@ -0,0 +1,8 @@
+package com.github.allepilli.odoodevelopmentplugin
+
+import com.intellij.openapi.util.IconLoader
+
+object OdooIcons {
+    @JvmField
+    val odoo = IconLoader.getIcon("/icons/odoo_icon.svg", javaClass)
+}
