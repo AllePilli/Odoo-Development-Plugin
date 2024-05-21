730fb939b2e33ae1bb1b837f35d07e276268c0b0
AllePilli <peal@odoo.com>
2024-05-21 22:31:53 +0200

    [IMP] model in self.env references
    for text instances of self.env[~literal_string~] in python files

--- src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/references/python/model_in_env/ModelInEnvValuesContributor.kt
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/references/python/model_in_env/ModelInEnvValuesContributor.kt
@@ -7,11 +7,15 @@ import com.jetbrains.python.psi.PyReferenceExpression
 import com.jetbrains.python.psi.PyStringLiteralExpression
 import com.jetbrains.python.psi.PySubscriptionExpression
 
+private val selfDotEnvCaller = PlatformPatterns.psiElement(PyReferenceExpression::class.java) // env
+        .withChild(PlatformPatterns.psiElement(PyReferenceExpression::class.java)) // self
+
+private val getModuleExpression = PlatformPatterns.psiElement(PySubscriptionExpression::class.java)
+        .withChild(selfDotEnvCaller)
+
 private val pattern = PlatformPatterns.psiElement(PyStringLiteralExpression::class.java)
-        .withParent(
-                PlatformPatterns.psiElement(PySubscriptionExpression::class.java)
-                        .withChild(PlatformPatterns.psiElement(PyReferenceExpression::class.java))
-        )
+        .withParent(getModuleExpression)
+
 
 class ModelInEnvValuesContributor: PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) = registrar
