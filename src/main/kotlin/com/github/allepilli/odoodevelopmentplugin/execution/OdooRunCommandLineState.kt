0493555e192eab4cbb27ac348711f76245db3f81
AllePilli <peal@odoo.com>
2024-07-19 17:16:59 +0200

    [ADD] Clear and readable log output for odoo run configuration

--- src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/execution/OdooRunCommandLineState.kt
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/execution/OdooRunCommandLineState.kt
@@ -1,22 +1,45 @@
 package com.github.allepilli.odoodevelopmentplugin.execution
 
 import com.github.allepilli.odoodevelopmentplugin.settings.general.GeneralSettingsState
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.filters.UrlFilter
+import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.target.TargetEnvironment
+import com.intellij.execution.target.value.TraceableTargetEnvironmentFunction
 import com.intellij.execution.ui.ConsoleView
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.encoding.EncodingProjectManager
-import com.jetbrains.python.run.PythonCommandLineState
-import com.jetbrains.python.run.PythonExecution
-import com.jetbrains.python.run.PythonScriptExecution
+import com.intellij.terminal.TerminalExecutionConsole
+import com.intellij.util.io.BaseOutputReader
+import com.jetbrains.python.run.*
 import com.jetbrains.python.run.target.HelpersAwareTargetEnvironmentRequest
 import java.io.File
 import java.nio.file.Path
+import java.util.function.Function
 
 @Suppress("UnstableApiUsage")
 class OdooRunCommandLineState(private val runConfiguration: OdooRunConfiguration, env: ExecutionEnvironment?) : PythonCommandLineState(runConfiguration, env) {
+    companion object {
+        private const val TERM_COLOR_STRING = "xterm-256color"
+        private val termColorFunction = object : TraceableTargetEnvironmentFunction<String>() {
+            override fun applyInner(t: TargetEnvironment): String = TERM_COLOR_STRING
+        }
+
+        private fun createTerminalExecutionConsole(project: Project, processHandler: ProcessHandler): TerminalExecutionConsole =
+                TerminalExecutionConsole(project, processHandler).apply {
+                    addMessageFilter(PythonTracebackFilter(project))
+                    addMessageFilter(UrlFilter())
+                }
+    }
+
     override fun buildPythonExecution(helpersAwareRequest: HelpersAwareTargetEnvironmentRequest): PythonExecution =
             PythonScriptExecution().apply {
                 val odooBin = File(runConfiguration.odooBinPath)
@@ -51,9 +74,45 @@ class OdooRunCommandLineState(private val runConfiguration: OdooRunConfiguration
                 charset = EncodingProjectManager.getInstance(runConfiguration.project).defaultCharset
             }
 
+    private fun executeWithCMDEmulation(processHandler: ProcessHandler): ExecutionResult {
+        val executeConsole = createTerminalExecutionConsole(runConfiguration.project, processHandler)
+        processHandler.startNotify()
+
+        return DefaultExecutionResult(executeConsole, processHandler, *createActions(executeConsole, processHandler))
+    }
+
+    override fun execute(executor: Executor?, processStarter: PythonProcessStarter?, vararg patchers: CommandLinePatcher?): ExecutionResult {
+        setRunWithPty(true)
+
+        val processHandler = startProcess(processStarter, *patchers)
+        return executeWithCMDEmulation(processHandler)
+    }
+
+    override fun execute(executor: Executor, converter: PythonScriptTargetedCommandLineBuilder): ExecutionResult? {
+        setRunWithPty(true)
+
+        val processHandler = startProcess(converter)
+        return executeWithCMDEmulation(processHandler)
+    }
+
     override fun createActions(console: ConsoleView?, processHandler: ProcessHandler?, executor: Executor?): Array<out AnAction?> {
         val superActions = super.createActions(console, processHandler, executor)
         val openDBAction = ActionManager.getInstance().getAction("OpenDbAction") ?: return superActions
         return superActions + openDBAction
     }
+
+    override fun customizeEnvironmentVars(envs: MutableMap<String, String>?, passParentEnvs: Boolean) {
+        super.customizeEnvironmentVars(envs, passParentEnvs)
+        if (!SystemInfo.isWindows) envs?.put("TERM", TERM_COLOR_STRING)
+    }
+
+    override fun customizePythonExecutionEnvironmentVars(helpersAwareTargetRequest: HelpersAwareTargetEnvironmentRequest, envs: MutableMap<String, Function<TargetEnvironment, String>>, passParentEnvs: Boolean) {
+        super.customizePythonExecutionEnvironmentVars(helpersAwareTargetRequest, envs, passParentEnvs)
+        if (!SystemInfo.isWindows) envs["TERM"] = termColorFunction
+    }
+
+    override fun doCreateProcess(commandLine: GeneralCommandLine): ProcessHandler =
+            object: OSProcessHandler(commandLine) {
+                override fun readerOptions(): BaseOutputReader.Options = BaseOutputReader.Options.forTerminalPtyProcess()
+            }
 }
