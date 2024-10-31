b9841536ccad2f37cf8a0e7deb7f2abfda641f35
AllePilli <peal@odoo.com>
2024-10-31 14:56:17 +0100

    [FIX] Module cannot depend on itself
    We don't add a completion result for the current models when adding dependencies to a module

--- src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/references/ModuleReference.kt
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/references/ModuleReference.kt
@@ -2,12 +2,18 @@ package com.github.allepilli.odoodevelopmentplugin.references
 
 import com.github.allepilli.odoodevelopmentplugin.OdooIcons
 import com.github.allepilli.odoodevelopmentplugin.directories
+import com.github.allepilli.odoodevelopmentplugin.extensions.addonPaths
+import com.github.allepilli.odoodevelopmentplugin.extensions.getContainingModule
 import com.github.allepilli.odoodevelopmentplugin.settings.general.GeneralSettingsState
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.findDirectory
+import com.intellij.openapi.vfs.originalFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiReferenceBase
@@ -36,11 +42,28 @@ class ModuleReference(psiElement: PsiElement, rangeInElement: TextRange): PsiRef
         val psiManager = PsiManager.getInstance(element.project)
         val virtualFileManager = VirtualFileManager.getInstance()
 
-        return GeneralSettingsState.getInstance(element.project).addonPaths
-                .asSequence()
+        // Can't use PsiElement.containingModule because the element in this function is some kind of temporary element with minimal context.
+        // to get the actual containing module we need to go through the document
+        val moduleName = if (ApplicationManager.getApplication().isUnitTestMode) {
+            FileDocumentManager.getInstance().getFile(element.containingFile.fileDocument)
+                    ?.originalFile()
+                    ?.parent
+                    ?.name
+        } else {
+            FileDocumentManager.getInstance().getFile(element.containingFile.fileDocument)
+                    ?.originalFile()
+                    ?.getContainingModule(element.project)
+                    ?.name
+        } ?: kotlin.run {
+            logger<ModuleReference>().error("Could not find the current module")
+            return emptyArray()
+        }
+
+        return element.project.addonPaths.asSequence()
                 .map { File(it).toPath() }
                 .mapNotNull { virtualFileManager.findFileByNioPath(it) }
                 .flatMap { it.directories }
+                .filterNot { it.name == moduleName }
                 .mapNotNull { vf -> psiManager.findDirectory(vf) }
                 .map { module -> LookupElementBuilder.create(module).withIcon(OdooIcons.odoo) }
                 .toList()
