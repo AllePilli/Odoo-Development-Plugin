7da76c1e30a3a7b191e993d27089f62e40a7199b
AllePilli <peal@odoo.com>
2025-06-20 18:13:18 +0200

    IMP: Translated Field Names in Data Files
    Since Odoo version 18.2 fields can be translated directly in data files. this commit adds completion and navigation for these translated field names.

--- src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/services/OdooVersionManager.kt
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/services/OdooVersionManager.kt
@@ -2,6 +2,7 @@ package com.github.allepilli.odoodevelopmentplugin.services
 
 import com.intellij.dvcs.repo.VcsRepositoryManager
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
@@ -28,15 +29,18 @@ class OdooVersionManager(private val project: Project) {
                         ?.let { "master" }
     }
 
-    fun getVersion(virtualFile: VirtualFile): String? {
+    fun getVersion(virtualFile: VirtualFile): OdooVersion? {
         val repositoryManager = VcsRepositoryManager.getInstance(project)
         val branchName = repositoryManager.getRepositoryForFile(virtualFile)
                 ?.currentBranchName
-                ?: return null
-        return getVersionString(branchName)
+                ?: kotlin.run {
+                    logger<OdooVersionManager>().warn("Could not find Odoo version for file: ${virtualFile.path}")
+                    return null
+                }
+        return getVersionString(branchName)?.let(OdooVersion.Companion::from)
     }
 
-    fun getVersion(): String? = VcsRepositoryManager.getInstance(project)
+    fun getVersion(): OdooVersion? = VcsRepositoryManager.getInstance(project)
             .getRepositories()
             .firstOrNull { repository ->
                 // find the odoo base repository
@@ -44,4 +48,68 @@ class OdooVersionManager(private val project: Project) {
             }
             ?.currentBranchName
             ?.let(::getVersionString)
+            ?.let(OdooVersion.Companion::from)
+
+    /**
+     * @return true if the current Odoo version >= [minVersion] false if the version could not be found or the condition fails
+     */
+    fun versionAtLeast(virtualFile: VirtualFile, minVersion: OdooVersion): Boolean {
+        val version = getVersion(virtualFile) ?: return false
+        return version >= minVersion
+    }
+
+    fun versionAtLeast(virtualFile: VirtualFile, minVersion: String): Boolean =
+            versionAtLeast(virtualFile, OdooVersion.from(minVersion))
+
+    /**
+     * @return true if the current Odoo version >= [minVersion] false if the version could not be found or the condition fails
+     */
+    fun versionAtLeast(minVersion: OdooVersion): Boolean {
+        val version = getVersion() ?: return false
+        return version >= minVersion
+    }
+
+    fun versionAtLeast(minVersion: String): Boolean = versionAtLeast(OdooVersion.from(minVersion))
+}
+
+sealed class OdooVersion: Comparable<OdooVersion> {
+    companion object {
+        fun from(versionString: String) = if (versionString == "master") Master else Numbered(versionString)
+    }
+
+    abstract val version: String
+
+    data object Master: OdooVersion() {
+        override val version: String
+            get() = "master"
+
+        override fun compareTo(other: OdooVersion): Int = when (other) {
+            is Master -> 0
+            is Numbered -> 1
+        }
+    }
+
+    class Numbered(override val version: String): OdooVersion() {
+        val majorVersion: Int
+        val minorVersion: Int
+
+        init {
+            if ('.' in version) {
+                val (major, minor) = version.split('.', limit = 2)
+                majorVersion = major.toInt()
+                minorVersion = minor.toInt()
+            } else {
+                majorVersion = version.toInt()
+                minorVersion = 0
+            }
+        }
+
+        override fun compareTo(other: OdooVersion): Int = when (other) {
+            is Master -> -1
+            is Numbered -> when (val majorComparison = majorVersion.compareTo(other.majorVersion)) {
+                0 -> minorVersion.compareTo(other.minorVersion)
+                else -> majorComparison
+            }
+        }
+    }
 }
--- src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/xml/dom/odoo_data_file/converters/SimpleFieldReferenceConverter.kt
+++ src/main/kotlin/com/github/allepilli/odoodevelopmentplugin/xml/dom/odoo_data_file/converters/SimpleFieldReferenceConverter.kt
@@ -1,6 +1,10 @@
 package com.github.allepilli.odoodevelopmentplugin.xml.dom.odoo_data_file.converters
 
+import com.github.allepilli.odoodevelopmentplugin.VersionConstants
 import com.github.allepilli.odoodevelopmentplugin.references.SimpleFieldNameReference
+import com.github.allepilli.odoodevelopmentplugin.services.OdooVersionManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.util.xml.ConvertContext
@@ -14,7 +18,17 @@ class SimpleFieldReferenceConverter: CustomReferenceConverter<String> {
 
         if (genericDomValue is GenericAttributeValue<String>) {
             val value = genericDomValue.xmlAttributeValue ?: return emptyArray()
-            return arrayOf(SimpleFieldNameReference(value))
+
+            val virtualFile = value.containingFile.virtualFile
+            val odooVersionManager = value.project.service<OdooVersionManager>()
+            val reference = if (odooVersionManager.versionAtLeast(virtualFile, VersionConstants.L18N_FIELD_NAMES)) {
+                // an '@' in the field name indicates that it is a translated field
+                val atIdx = value.value.indexOf('@')
+                if (atIdx != -1) SimpleFieldNameReference(value, TextRange.create(1, atIdx + 1))
+                else SimpleFieldNameReference(value)
+            } else SimpleFieldNameReference(value)
+
+            return arrayOf(reference)
         }
 
         return emptyArray()
